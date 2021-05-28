e755afcd90a4531a2f5f9771b700b787955b930e
jagadeesh kakuluri <jagadeeshkakuluri@gmail.com>
2021-05-28 20:00:24 +0530

    refactored

--- src/com/interview/array/BinarySearch.java
+++ src/com/interview/array/BinarySearch.java
@@ -3,6 +3,6 @@ package com.interview.array;
 public class BinarySearch {
     public static void main(String[] args) {
         // write your code here
-        System.out.println("hello world");
+        System.out.println("hello world naveen");
     }
 }
